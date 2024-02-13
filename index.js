86e83249281b38b0d4713f95bfea3aad6db76e25
Magnus Spånggård <magnus@detecht.se>
2024-02-13 15:52:56 +0100

    use node20

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
+const core = require("@actions/core")
+const fs = require("fs")
 
 // versionCode — A positive integer [...] -> https://developer.android.com/studio/publish/versioning
-const versionCodeRegexPattern = /(versionCode(?:\s|=)*)(.*)/;
+const versionCodeRegexPattern = /(versionCode(?:\s|=)*)(.*)/
 // versionName — A string used as the version number shown to users [...] -> https://developer.android.com/studio/publish/versioning
-const versionNameRegexPattern = /(versionName(?:\s|=)*)(.*)/;
+const versionNameRegexPattern = /(versionName(?:\s|=)*)(.*)/
 
 try {
-    const gradlePath = core.getInput('gradlePath');
-    const versionCode = core.getInput('versionCode');
-    const versionName = core.getInput('versionName');
-    console.log(`Gradle Path : ${gradlePath}`);
-    console.log(`Version Code : ${versionCode}`);
-    console.log(`Version Name : ${versionName}`);
+  const gradlePath = core.getInput("gradlePath")
+  const versionCode = core.getInput("versionCode")
+  const versionName = core.getInput("versionName")
+  console.log(`Gradle Path : ${gradlePath}`)
+  console.log(`Version Code : ${versionCode}`)
+  console.log(`Version Name : ${versionName}`)
 
-    fs.readFile(gradlePath, 'utf8', function (err, data) {
-        newGradle = data;
-        if (versionCode.length > 0)
-            newGradle = newGradle.replace(versionCodeRegexPattern, `$1${versionCode}`);
-        if (versionName.length > 0)
-            newGradle = newGradle.replace(versionNameRegexPattern, `$1\"${versionName}\"`);
-        fs.writeFile(gradlePath, newGradle, function (err) {
-            if (err) throw err;
-            if (versionCode.length > 0)
-                console.log(`Successfully override version code ${versionCode}`)
-            if (versionName.length > 0)
-                console.log(`Successfully override version code ${versionName}`)
-            core.setOutput("result", `Done`);
-        });
-    });
-
+  fs.readFile(gradlePath, "utf8", function (err, data) {
+    newGradle = data
+    if (versionCode.length > 0)
+      newGradle = newGradle.replace(versionCodeRegexPattern, `$1${versionCode}`)
+    if (versionName.length > 0)
+      newGradle = newGradle.replace(
+        versionNameRegexPattern,
+        `$1\"${versionName}\"`
+      )
+    fs.writeFile(gradlePath, newGradle, function (err) {
+      if (err) throw err
+      if (versionCode.length > 0)
+        console.log(`Successfully override version code ${versionCode}`)
+      if (versionName.length > 0)
+        console.log(`Successfully override version code ${versionName}`)
+      core.setOutput("result", `Done`)
+    })
+  })
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message)
 }
